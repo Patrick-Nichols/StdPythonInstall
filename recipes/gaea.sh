d597ca95f406bc6740fbece8f7543f3a0e772e78
patrick.nichols@noaa.gov <patrick.nichols@noaa.gov>
2026-03-24 17:23:45 -0400

    put in changes for gaea install

--- recipes/gaea.sh
+++ recipes/gaea.sh
@@ -1,3 +1,6 @@
-
-/ncrc/usw/rdhpcs/software
-/ncrc/usw/rdhpcs/
+sudo su - role.apps
+cd /home/role.apps
+git clone
+cd StdPythonInstall
+bash install_ps.sh /autofs/ncrc-svm1_usw/rdhpcs/software /autofs/ncrc-svm1_usw
+module use /ncrc/usw/rdhpcs/modulefiles/
